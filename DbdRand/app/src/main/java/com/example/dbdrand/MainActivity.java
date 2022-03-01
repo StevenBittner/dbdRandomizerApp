d44e2e95462ac1270e301025428e0f9605ab9ca8
Steven Bittner <steven.bittner@haw-hamburg.de>
2022-03-01 18:12:39 +0100

    added activity for killer and survivor, first layout for survivor

--- DbdRand/app/src/main/java/com/example/dbdrand/MainActivity.java
+++ DbdRand/app/src/main/java/com/example/dbdrand/MainActivity.java
@@ -2,53 +2,37 @@ package com.example.dbdrand;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    String msg = "Android : ";
+    ImageButton survivorButton, killerButton;
 
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(msg, "The onCreate() event");
+
+        survivorButton = (ImageButton) findViewById(R.id.SurvivorButton);
+        killerButton = (ImageButton) findViewById(R.id.KillerButton);
+
+        survivorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SurvivorActivity.class));
+            }
+        });
+
+        killerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, KillerActivity.class));
+            }
+        });
     }
 
-    /** Called when the activity is about to become visible. */
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(msg, "The onStart() event");
-    }
 
-    /** Called when the activity has become visible. */
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(msg, "The onResume() event");
-    }
-
-    /** Called when another activity is taking focus. */
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(msg, "The onPause() event");
-    }
-
-    /** Called when the activity is no longer visible. */
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(msg, "The onStop() event");
-    }
-
-    /** Called just before the activity is destroyed. */
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.d(msg, "The onDestroy() event");
-    }
 }
